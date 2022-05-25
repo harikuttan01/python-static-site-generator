acb0c2679a4d6aad6617c38bab268598bd655270
harikuttan01 <harikuttan01@users.noreply.github.com>
2022-05-25 22:35:10 +0530

    Completed module

---
+++ ssg/parsers.py
@@ -0,0 +1,36 @@
+from typing import List
+import shutil
+from pathlib import Path
+
+class Parser:
+    
+    def __init__(self):
+        self.extensions = [str]
+
+    def valid_extension(self,extension):
+        return True if extension in self.extensions else False
+    
+    def parse(self,source,dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+        raise NotImplementedError
+
+    def read(self,path):
+        with open(path,'r') as file:
+            f = file.read()
+            return f
+    def write(self,path,dest,content,ext = ".html"):
+        full_path = self.dest / path.with_suffix(ext).name
+        with open(full_path,'w') as file:
+            file.write(content)
+
+    def copy(self,path,source,dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
+class ResourceParser(Parser):
+
+    EXTENSIONS = ['.jpg','.png','.gif','.css','.hmtl']
+    def parse(self,source,dest,path):
+        super().copy(path,source,dest)
+        
